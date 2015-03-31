d0eea15e6e340b322899ca8bc2ace954201562a6
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-03-31 17:13:16 +0300

    added improved problem 01 solution

--- week2/1-CollectionsAndGenerics1/src/P01_CheckBrachetsExpression.java
+++ week2/1-CollectionsAndGenerics1/src/P01_CheckBrachetsExpression.java
@@ -9,7 +9,7 @@ public class P01_CheckBrachetsExpression {
 //		Scanner input = new Scanner(System.in);
 //	    String bracketsString = input.nextLine();
 	    
-	    // hardcoded input
+	    // hard coded input
 		// I get the input as a string
 		// because (according to multiple articles in SO)
 		// iteration over str.charAt(i); is faster than
@@ -21,35 +21,41 @@ public class P01_CheckBrachetsExpression {
 	    // initial fast checks
 	    // if the brackets are even number
 	    if	(input.length() % 2 == 0) {
-	    	String lastChar = (input.substring(input.length() - 1));
-	    	
 	    	// initial fast checks
 		    // if the last bracket is of correct type
-	    	if (lastChar.equals(")")) {
-	    		
-	    		// if initial checks are o.k. then we iterate
-	    		for (int i = 0, n = input.length(); i < n; i++) {
-			        char c = input.charAt(i);
-			        if (c == '(') {
-			        	counter++;
-			        } else if (c == ')') {
-			        	counter--;
-			        } else {
-			        	System.out.println(INVALID_INPUT_MESSAGE);
-			        	break;
-			        }
-			        
-			        if (counter < 0) {
-			        	System.out.println(NOT_CORRECT_EXPRESSION_MESSAGE);
-			        	break;
-			        }
-			    }
-	    	} else {
-	    		System.out.println(NOT_CORRECT_EXPRESSION_MESSAGE);
-	    	}
+	    	String lastChar = (input.substring(input.length() - 1));
+    		
+    		// if initial checks are o.k. then we iterate
+	    	iterateCharArray(INVALID_INPUT_MESSAGE,
+					NOT_CORRECT_EXPRESSION_MESSAGE, input, counter, lastChar);
 
 	    } else {
 	    	System.out.println(NOT_CORRECT_EXPRESSION_MESSAGE);
 	    }    
+	}
+
+	private static void iterateCharArray(final String INVALID_INPUT_MESSAGE,
+			final String NOT_CORRECT_EXPRESSION_MESSAGE, String input,
+			int counter, String lastChar) {
+		if (lastChar.equals(")")) {
+			for (int i = 0, n = input.length(); i < n; i++) {
+		        char c = input.charAt(i);
+		        if (c == '(') {
+		        	counter++;
+		        } else if (c == ')') {
+		        	counter--;
+		        	if (counter < 0) {
+			        	System.out.println(NOT_CORRECT_EXPRESSION_MESSAGE);
+			        	break;
+			        }
+		        } else {
+		        	System.out.println(INVALID_INPUT_MESSAGE);
+		        	break;
+		        }			      
+		    }
+			
+		} else {
+			System.out.println(NOT_CORRECT_EXPRESSION_MESSAGE);
+		}
 	}
 }
